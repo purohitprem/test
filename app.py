23dca56f377e662a031942a84c43b39ea16a2a5a
prempurohit260 <prempurohit260@gmail.com>
2026-06-10 01:19:26 +0530

    A curd opration with using fastapi and uvicorn

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-import flask
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
-app = flask.Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
+class tea(BaseModel):
+    id :int 
+    name : str
+    origin : str 
+
+teas: list[tea]=[]
+
+
+@app.get("/")
 def index():
-    return "this is index page"
+    return {"messege":"this is Tea House"}
 
-app.run( debug=True)
+@app.get("/teas")
+def get_teas():
+    return teas
+
+@app.post("/teas")
+def add_teas(tea : tea):
+    teas.append(tea)
+    return teas
+
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id:int, updated_tea :tea):
+    for index, tea in enumerate(teas):
+        if tea.id==tea_id:
+            teas[index] = updated_tea
+            return updated_tea
+    return {"error": "Tea not found"}
+
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id :int):
+    for index, tea in enumerate(teas):
+        if tea.id==tea_id:
+            delete = teas.pop(index)
+            return delete
+    return {"error": "Tea not found"}
